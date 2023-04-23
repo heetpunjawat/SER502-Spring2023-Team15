056a8d4ffd4d5afcd1166f3778029dc4b4fed0c4
Heet Punjawat <hpunjawa@asu.edu>
2023-04-22 19:27:47 -0700

    Create parser.pl

---
+++ src/parser.pl
@@ -0,0 +1,23 @@
+/* Start Symbol */
+program(t_program(P)) --> statement_list(P).
+
+/* Blocks */
+block(t_block(StatementList)) --> ['{'], statement_list(StatementList), ['}'].
+
+/* Statements List  */
+statement_list(t_statement_list(Statement, StatementList)) -->
+    statement(Statement),
+    statement_list(StatementList).
+
+statement_list(t_statement(Statement)) -->
+    statement(Statement).
+
+/* Statements */
+statement(S) -->
+    print_statement(S) |
+    assign_statement(S) |
+    var_decl_statement(S) |
+    if_statement(S) |
+    for_loop_statement(S) | 
+    for_enhanced_statement(S) |
+    while_loop_statement(S).
