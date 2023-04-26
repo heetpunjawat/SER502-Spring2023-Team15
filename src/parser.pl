85ffc7fdc30be67e37292c5a095df30586846ee0
Heet Punjawat <hpunjawa@asu.edu>
2023-04-26 16:04:34 -0700

    Updating Grammar
    Making "operator" and "operators" corrections as suggested in submission comment of Project Milestone 1

--- src/parser.pl
+++ src/parser.pl
@@ -1,8 +1,10 @@
 /* Start Symbol */
 program(t_program(P)) --> statement_list(P).
 
+
 /* Blocks */
 block(t_block(StatementList)) --> ['{'], statement_list(StatementList), ['}'].
+
 
 /* Statements List  */
 statement_list(t_statement_list(Statement, StatementList)) -->
@@ -11,6 +13,7 @@ statement_list(t_statement_list(Statement, StatementList)) -->
 
 statement_list(t_statement(Statement)) -->
     statement(Statement).
+
 
 /* Statements */
 statement(S) -->
@@ -22,14 +25,17 @@ statement(S) -->
     for_enhanced_statement(S) |
     while_loop_statement(S).
 
+
 /* If, Elif, Else Statements */
 if_statement(t_if_statement(IfLoop, ElifLoop, ElseLoop)) -->
     if_clause(IfLoop),
     elif_clause(ElifLoop),
     else_clause(ElseLoop).
+
 if_statement(t_if_statement(IfLoop, ElseLoop)) -->
     if_clause(IfLoop),
     else_clause(ElseLoop).
+
 if_statement(t_if_statement(IfLoop)) -->
     if_clause(IfLoop).
 
@@ -40,12 +46,14 @@ if_clause(t_if(Condition, Block)) -->
     condition(Condition),
     [')'],
     block(Block).
+
 elif_clause(t_elif(Condition, Block)) -->
     [elif],
     ['('],
     condition(Condition),
     [')'],
     block(Block).
+
 elif_clause(t_elif(Condition, Block, Elifclause)) -->
     [elif],
     ['('],
@@ -53,11 +61,15 @@ elif_clause(t_elif(Condition, Block, Elifclause)) -->
     [')'],
     block(Block),
     elif_clause(Elifclause).
+
 else_clause(t_else(Block)) -->
     [else],
     block(Block).
 
+
 /* Print statements */
 print_statement(t_print_str(Value)) --> [print_str], ['('], string_val(t_string(Value)), [')'], end_of_statement(_).
+
 print_statement(t_print_str(Value)) --> [print_str], ['('], var_name(t_var_name(Value)), [')'], end_of_statement(_).
+
 print_statement(t_print_expr(Expression)) --> [print_expr], ['('], expression(Expression), [')'], end_of_statement(_).
