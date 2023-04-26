352ce08b4ebd4f7a8f427b7a13312e9dda3c754e
Heet Punjawat <hpunjawa@asu.edu>
2023-04-26 14:27:14 -0700

    Update parser.pl
    Adding if, elif, else statements and clauses, and adding print statements

--- src/parser.pl
+++ src/parser.pl
@@ -1,6 +1,5 @@
 /* Start Symbol */
-program(p_program(P)) --> statement_list(P).
-program --> statement_list.
+program(t_program(P)) --> statement_list(P).
 
 /* Blocks */
 block(t_block(StatementList)) --> ['{'], statement_list(StatementList), ['}'].
@@ -22,3 +21,43 @@ statement(S) -->
     for_loop_statement(S) | 
     for_enhanced_statement(S) |
     while_loop_statement(S).
+
+/* If, Elif, Else Statements */
+if_statement(t_if_statement(IfLoop, ElifLoop, ElseLoop)) -->
+    if_clause(IfLoop),
+    elif_clause(ElifLoop),
+    else_clause(ElseLoop).
+if_statement(t_if_statement(IfLoop, ElseLoop)) -->
+    if_clause(IfLoop),
+    else_clause(ElseLoop).
+if_statement(t_if_statement(IfLoop)) -->
+    if_clause(IfLoop).
+
+/* If, Elif, Else Clauses */
+if_clause(t_if(Condition, Block)) -->
+    [if],
+    ['('],
+    condition(Condition),
+    [')'],
+    block(Block).
+elif_clause(t_elif(Condition, Block)) -->
+    [elif],
+    ['('],
+    condition(Condition),
+    [')'],
+    block(Block).
+elif_clause(t_elif(Condition, Block, Elifclause)) -->
+    [elif],
+    ['('],
+    condition(Condition),
+    [')'],
+    block(Block),
+    elif_clause(Elifclause).
+else_clause(t_else(Block)) -->
+    [else],
+    block(Block).
+
+/* Print statements */
+print_statement(t_print_str(Value)) --> [print_str], ['('], string_val(t_string(Value)), [')'], end_of_statement(_).
+print_statement(t_print_str(Value)) --> [print_str], ['('], var_name(t_var_name(Value)), [')'], end_of_statement(_).
+print_statement(t_print_expr(Expression)) --> [print_expr], ['('], expression(Expression), [')'], end_of_statement(_).
